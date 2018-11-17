1daf735c69893549b5f736e0cd67aa74c2f0d05f
zhuxiujia <zhuxiujia@qq.com>
2018-11-17 16:17:42 +0800

    完善事务模型

--- example/Example_test.go
+++ example/Example_test.go
@@ -66,7 +66,7 @@ func Test_Remote_Transation(t *testing.T) {
 	//本地连接
 	var err error
 	//mysql链接格式为         用户名:密码@(数据库链接地址:端口)/数据库名称   例如root:123456@(***.mysql.rds.aliyuncs.com:3306)/test
-	engine, err := GoMybatis.OpenRemote(addr) //此处请按格式填写你的mysql链接，这里用*号代替
+	engine, err := GoMybatis.OpenRemote(addr) //远程服务地址
 	if err != nil {
 		panic(err.Error())
 	}
