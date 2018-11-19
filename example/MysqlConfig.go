ff9b23cf5bb0b99bc26c2dedebf6a01c083757cc
zhuxiujia <zhuxiujia@qq.com>
2018-11-19 16:59:36 +0800

    fix bug

--- example/MysqlConfig.go
+++ example/MysqlConfig.go
@@ -2,7 +2,7 @@ package example
 
 //mysql链接格式为         用户名:密码@(数据库链接地址:端口)/数据库名称   例如root:123456@(***.mysql.rds.aliyuncs.com:3306)/test
 //此处请按格式填写你的mysql链接，这里用*号代替
-const MysqlUri="root:123!@#TEST@(rm-bp1149af62y4hnljoo.mysql.rds.aliyuncs.com:3306)/test?charset=utf8&parseTime=True&loc=Local"
+const MysqlUri="*"
 
 const MysqlDriverName  = "mysql"
 
