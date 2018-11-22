675def9c7fa71c116eb34899f6e0eed26f6873e7
zhuxiujia <zhuxiujia@qq.com>
2018-11-22 14:07:17 +0800

    附加测试sql数据

--- example/database.sql
+++ example/database.sql
@@ -1,3 +1,4 @@
+DROP TABLE IF EXISTS `biz_activity`;
 CREATE TABLE `biz_activity` (
   `id` varchar(255) NOT NULL,
   `name` varchar(255) NOT NULL,
@@ -8,4 +9,14 @@ CREATE TABLE `biz_activity` (
   `create_time` datetime NOT NULL,
   `delete_flag` int(1) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 ROW_FORMAT=DYNAMIC COMMENT='Example';
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 ROW_FORMAT=DYNAMIC COMMENT='test';
+
+-- ----------------------------
+-- Records of biz_activity
+-- ----------------------------
+INSERT INTO `biz_activity` VALUES ('165', '安利一波大表哥', null, 'http://www.baidu.com', 'http://www.taobao.com', 'ceshi', '2018-05-23 15:21:22', '0');
+INSERT INTO `biz_activity` VALUES ('166', '注册送好礼', null, '', 'www.baidu.com', '测试', '2018-05-24 10:36:31', '0');
+INSERT INTO `biz_activity` VALUES ('167', 'hello', null, 'www.baidu.com', 'www.baidu.com', 'ceshi', '2018-05-24 10:41:17', '0');
+INSERT INTO `biz_activity` VALUES ('168', 'rs168', null, null, null, null, '0000-00-00 00:00:00', '0');
+INSERT INTO `biz_activity` VALUES ('169', 'rs168', null, null, null, null, '0000-00-00 00:00:00', '0');
+INSERT INTO `biz_activity` VALUES ('170', 'rs168-10', null, null, null, null, '0000-00-00 00:00:00', '1');
