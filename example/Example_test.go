52ea1dcdd8890c74414f1801ac7b0931c64e4caa
zhuxiujia <zhuxiujia@qq.com>
2018-11-23 15:49:03 +0800

    添加foreach例子/example/Example_test.go

--- example/Example_test.go
+++ example/Example_test.go
@@ -16,6 +16,7 @@ import (
 //参数中除了session指针外，为指针类型的皆为返回数据
 // 函数return必须为error 为返回错误信息
 type ExampleActivityMapperImpl struct {
+	SelectByIds       func(ids []string,result *[]Activity) error `mapperParams:"ids"`
 	SelectAll         func(result *[]Activity) error
 	SelectByCondition func(name string, startTime time.Time, endTime time.Time, page int, size int, result *[]Activity) error `mapperParams:"name,startTime,endTime,page,size"`
 	UpdateById        func(session *GoMybatis.Session, arg Activity, result *int64) error                                     //只要参数中包含有*GoMybatis.Session的类型，框架默认使用传入的session对象，用于自定义事务
@@ -52,6 +53,21 @@ func Test_main(t *testing.T) {
 	//使用mapper
 	var result []Activity
 	var err = exampleActivityMapperImpl.SelectByCondition("", time.Time{}, time.Time{}, 0, 2000, &result)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println("result=", result)
+}
+
+//本地GoMybatis使用例子
+func Test_ForEach(t *testing.T) {
+	//初始化mapper文件
+	var exampleActivityMapperImpl = InitMapperByLocalSession()
+	//使用mapper
+	var result []Activity
+
+	var ids=[]string{"170"}
+	var err = exampleActivityMapperImpl.SelectByIds(ids, &result)
 	if err != nil {
 		panic(err)
 	}
