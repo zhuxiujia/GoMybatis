54a6d17be6242b13586e9feb63d16c2f3576a443
zhuxiujia <zhuxiujia@qq.com>
2018-12-06 20:03:52 +0800

    add choose when otherwise support

--- example/MysqlConfig.go
+++ example/MysqlConfig.go
@@ -2,6 +2,6 @@ package example
 
 //mysql链接格式为         用户名:密码@(数据库链接地址:端口)/数据库名称   例如root:123456@(***.mysql.rds.aliyuncs.com:3306)/test
 //此处请按格式填写你的mysql链接，这里用*号代替
-const MysqlUri = "*s"
+const MysqlUri = "*"
 
 const MysqlDriverName = "mysql"
