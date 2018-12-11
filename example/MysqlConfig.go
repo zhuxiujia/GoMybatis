50b5fc908c8c6229190f1b5923dc5a788a014dfb
zhuxiujia <zhuxiujia@qq.com>
2018-12-11 18:07:22 +0800

    测试数据

--- example/MysqlConfig.go
+++ example/MysqlConfig.go
@@ -1,6 +1,6 @@
 package example
 
-//mysql链接格式为         用户名:密码@(数据库链接地址:端口)/数据库名称   例如root:123456@(***.mysql.rds.aliyuncs.com:3306)/test
+//mysql链接格式为         用户名:密码@(数据库链接地址:端口)/数据库名称   例如root:root@(127.0.0.1:3306)/test?charset=utf8&parseTime=True&loc=Local
 //此处请按格式填写你的mysql链接，这里用*号代替
 const MysqlUri = "*"
 
