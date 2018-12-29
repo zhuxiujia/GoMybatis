d8130c6fb0929cfdef8b4d89c73594e1faacf592
zhuxiujia <zhuxiujia@qq.com>
2018-12-29 20:56:02 +0800

    add test code

--- TypeConvertAdapter_test.go
+++ TypeConvertAdapter_test.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"reflect"
 	"testing"
+	"time"
 )
 
 func Test_ExpressionTypeConvert(t *testing.T) {
@@ -13,7 +14,31 @@ func Test_ExpressionTypeConvert(t *testing.T) {
 		Type:  reflect.TypeOf(a),
 	})
 	if convertResult != true {
-		t.Fatal(`Test_Adapter fail convertResult != true`)
+		t.Fatal(`Test_ExpressionTypeConvert fail`)
+	}
+	fmt.Println(convertResult)
+	convertResult = GoMybatisExpressionTypeConvert{}.Convert(SqlArg{
+		Value: 1,
+		Type:  reflect.TypeOf(1),
+	})
+	if convertResult == nil {
+		t.Fatal(`Test_ExpressionTypeConvert fail`)
+	}
+	fmt.Println(convertResult)
+	convertResult = GoMybatisExpressionTypeConvert{}.Convert(SqlArg{
+		Value: time.Now(),
+		Type:  reflect.TypeOf(time.Now()),
+	})
+	if convertResult == nil {
+		t.Fatal(`Test_ExpressionTypeConvert fail`)
+	}
+	fmt.Println(convertResult)
+	convertResult = GoMybatisExpressionTypeConvert{}.Convert(SqlArg{
+		Value: "string",
+		Type:  reflect.TypeOf("string"),
+	})
+	if convertResult == nil {
+		t.Fatal(`Test_ExpressionTypeConvert fail`)
 	}
 	fmt.Println(convertResult)
 }
@@ -26,6 +51,30 @@ func Test_SqlArgTypeConvert(t *testing.T) {
 	})
 	if convertResult == "" {
 		t.Fatal(`Test_Adapter fail convertResult != true`)
+	}
+	fmt.Println(convertResult)
+	convertResult = GoMybatisSqlArgTypeConvert{}.Convert(SqlArg{
+		Value: 1,
+		Type:  reflect.TypeOf(1),
+	})
+	if convertResult == "" {
+		t.Fatal(`Test_Adapter fail convertResult != 1`)
+	}
+	fmt.Println(convertResult)
+	convertResult = GoMybatisSqlArgTypeConvert{}.Convert(SqlArg{
+		Value: time.Now(),
+		Type:  reflect.TypeOf(time.Now()),
+	})
+	if convertResult == "" {
+		t.Fatal(`Test_Adapter fail convertResult != time.Time`)
+	}
+	fmt.Println(convertResult)
+	convertResult = GoMybatisSqlArgTypeConvert{}.Convert(SqlArg{
+		Value: "string",
+		Type:  reflect.TypeOf("string"),
+	})
+	if convertResult == "" {
+		t.Fatal(`Test_Adapter fail convertResult != string`)
 	}
 	fmt.Println(convertResult)
 }
