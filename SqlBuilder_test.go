bf64e4082a3e4aced76b76e2de6522e6333ceb86
zhuxiujia <zhuxiujia@qq.com>
2019-01-02 18:16:47 +0800

    add test code

--- SqlBuilder_test.go
+++ SqlBuilder_test.go
@@ -196,3 +196,55 @@ func Test_bind_string(t *testing.T) {
 	}
 	fmt.Println(result)
 }
+
+func TestGoMybatisSqlBuilder_BuildSql(t *testing.T) {
+	var mapper = `<?xml version="1.0" encoding="UTF-8"?>
+<!DOCTYPE mapper PUBLIC "-//mybatis.org//DTD Mapper 3.0//EN" "http://mybatis.org/dtd/mybatis-3-mapper.dtd">
+<mapper>
+    <!--List<Activity> selectByCondition(@Param("name") String name,@Param("startTime") Date startTime,@Param("endTime") Date endTime,@Param("index") Integer index,@Param("size") Integer size);-->
+    <!-- 后台查询产品 -->
+    <select id="selectByCondition">
+        select * from biz_activity where delete_flag=1
+        <if test="name != ''">
+            and name like concat('%',#{name},'%')
+        </if>
+        <if test="startTime != ''">
+            and create_time >= #{startTime}
+        </if>
+        <if test="endTime != ''">
+            and create_time &lt;= #{endTime}
+        </if>
+        order by create_time desc
+        <if test="page >= 0 and size != 0">limit #{page}, #{size}</if>
+    </select>
+</mapper>`
+	var mapperTree = LoadMapperXml([]byte(mapper))
+
+	var builder = GoMybatisSqlBuilder{}.New(GoMybatisExpressionTypeConvert{}, GoMybatisSqlArgTypeConvert{})
+	var paramMap = make(map[string]SqlArg)
+	paramMap["name"] = SqlArg{
+		Value: "",
+		Type:  reflect.TypeOf(""),
+	}
+	paramMap["startTime"] = SqlArg{
+		Value: "",
+		Type:  reflect.TypeOf(""),
+	}
+	paramMap["endTime"] = SqlArg{
+		Value: "",
+		Type:  reflect.TypeOf(""),
+	}
+	paramMap["page"] = SqlArg{
+		Value: 0,
+		Type:  reflect.TypeOf(0),
+	}
+	paramMap["size"] = SqlArg{
+		Value: 0,
+		Type:  reflect.TypeOf(0),
+	}
+	var sql, err = builder.BuildSql(paramMap, mapperTree["selectByCondition"], false)
+	if err != nil {
+		t.Fatal(err)
+	}
+	fmt.Println(sql)
+}
