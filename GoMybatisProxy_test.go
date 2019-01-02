34c36a2109a1415b4821683463698a96adaffd8b
zhuxiujia <zhuxiujia@qq.com>
2019-01-02 18:11:20 +0800

    add test code

--- GoMybatisProxy_test.go
+++ GoMybatisProxy_test.go
@@ -10,6 +10,32 @@ type TestMapper struct {
 	SelectByIds func(id string) (string, error) `mapperParams:"ids"`
 }
 
+func TestUseMapperValue(t *testing.T) {
+	var test = TestMapper{}
+	UseMapperValue(reflect.ValueOf(&test), func(method string, args []reflect.Value, tagArgs []TagArg) []reflect.Value {
+		if method == "" {
+			t.Fatal("UseMapper() method == ''")
+		}
+		if len(args) <= 0 {
+			t.Fatal("UseMapper() args len = 0")
+		}
+		if len(tagArgs) <= 0 {
+			t.Fatal("UseMapper() tagArgs len = 0")
+		}
+		var e error
+		var returns = make([]reflect.Value, 0)
+		returns = append(returns, reflect.ValueOf("yes return string="+args[0].Interface().(string)))
+		returns = append(returns, reflect.Zero(reflect.TypeOf(&e).Elem()))
+		return returns
+	})
+
+	var result, e = test.SelectByIds("1234")
+	fmt.Println(result, e)
+	if e != nil {
+		t.Fatal(e)
+	}
+}
+
 func TestUseMapper(t *testing.T) {
 	var test = TestMapper{}
 	UseMapper(&test, func(method string, args []reflect.Value, tagArgs []TagArg) []reflect.Value {
@@ -31,4 +57,7 @@ func TestUseMapper(t *testing.T) {
 
 	var result, e = test.SelectByIds("1234")
 	fmt.Println(result, e)
+	if e != nil {
+		t.Fatal(e)
+	}
 }
