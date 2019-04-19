324729c15e8be33f65bfc0b34a60ec129de4132b
ce <lzm420241@163.com>
2019-04-19 12:57:38 +0800

    add test

--- example/Aop_tx_test.go
+++ example/Aop_tx_test.go
@@ -8,29 +8,35 @@ import (
 
 type Service struct {
 	FindName func() (string, error)
+
+	SayHello func() (string, error)
 }
 
 func TestService(t *testing.T) {
-	var s = Service{
+	var s Service
+	s = Service{
 		FindName: func() (string, error) {
+			println("TestService")
+			s.SayHello()
 			return "TestService", nil
+		},
+		SayHello: func() (s string, e error) {
+			println("hello")
+			return "hello", nil
 		},
 	}
 	AopProxyService(&s)
-	var r, _ = s.FindName()
-	println("result:", r)
+	s.FindName()
 }
 
 func AopProxyService(service interface{}) {
-	GoMybatis.UseMapper(service, func(funcField reflect.StructField, field reflect.Value) func(arg GoMybatis.ProxyArg) []reflect.Value {
+	GoMybatis.AopProxy(service, func(funcField reflect.StructField, field reflect.Value) func(arg GoMybatis.ProxyArg) []reflect.Value {
 		//拷贝老方法，否则会循环调用导致栈溢出
 		var oldFunc = reflect.ValueOf(field.Interface())
 		var fn = func(arg GoMybatis.ProxyArg) []reflect.Value {
-			//befor
-
+			println("start:" + funcField.Name)
 			var oldFuncResults = oldFunc.Call(arg.Args)
-			//after
-
+			println("end:" + funcField.Name)
 			return oldFuncResults
 		}
 		return fn
