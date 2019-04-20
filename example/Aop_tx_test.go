cd59cc01d83e38981dbcf5aa353f7388e3f78fcc
ce <lzm420241@163.com>
2019-04-20 11:52:35 +0800

    rename method

--- example/Aop_tx_test.go
+++ example/Aop_tx_test.go
@@ -31,19 +31,55 @@ func TestTestService(t *testing.T) {
 
 func AopProxyService(service interface{}) {
 	//调用方法栈
+	var beanType = reflect.TypeOf(service).Elem()
+	var beanName = beanType.PkgPath() + beanType.Name()
+	var session GoMybatis.Session
 	var txStack = tx.StructField{}.New()
 	GoMybatis.Proxy(service, func(funcField reflect.StructField, field reflect.Value) func(arg GoMybatis.ProxyArg) []reflect.Value {
 		//拷贝老方法，否则会循环调用导致栈溢出
 		var nativeImplFunc = reflect.ValueOf(field.Interface())
 		var fn = func(arg GoMybatis.ProxyArg) []reflect.Value {
 			txStack.Push(funcField)
+			if txStack.Len() == 1 {
+				//PROPAGATION_REQUIRED
+				if session == nil {
+					//todo newSession is use service bean name?
+					session, _ = GoMybatis.GoMybatisEngine{}.NewSession(beanName)
+					session.Begin()
+				}
+			}
+			defer func() {
+				err := recover()
+				if err != nil {
+					session.Rollback()
+				}
+			}()
 			var nativeImplResult = nativeImplFunc.Call(arg.Args)
 			txStack.Pop()
 			if txStack.Len() == 0 {
-				//todo rollback
+				if haveError(nativeImplResult) {
+					session.Commit()
+				} else {
+					session.Rollback()
+				}
 			}
 			return nativeImplResult
 		}
 		return fn
 	})
+}
+
+func haveError(v []reflect.Value) bool {
+	if v == nil || len(v) == 0 {
+		return false
+	}
+	for _, item := range v {
+		if item.Kind() == reflect.Interface {
+			if item.String() == "error" {
+				if !item.IsNil() {
+					return true
+				}
+			}
+		}
+	}
 }
