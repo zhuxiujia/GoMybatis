3bb941a8557b9bdc5433a2be5008b730514be630
ce <lzm420241@163.com>
2019-04-20 11:28:12 +0800

    rename method

--- example/Aop_tx_test.go
+++ example/Aop_tx_test.go
@@ -34,15 +34,15 @@ func AopProxyService(service interface{}) {
 	var txStack = tx.StructField{}.New()
 	GoMybatis.Proxy(service, func(funcField reflect.StructField, field reflect.Value) func(arg GoMybatis.ProxyArg) []reflect.Value {
 		//拷贝老方法，否则会循环调用导致栈溢出
-		var oldFunc = reflect.ValueOf(field.Interface())
+		var nativeImplFunc = reflect.ValueOf(field.Interface())
 		var fn = func(arg GoMybatis.ProxyArg) []reflect.Value {
 			txStack.Push(funcField)
-			var oldFuncResults = oldFunc.Call(arg.Args)
+			var nativeImplResult = nativeImplFunc.Call(arg.Args)
 			txStack.Pop()
 			if txStack.Len() == 0 {
 				//todo rollback
 			}
-			return oldFuncResults
+			return nativeImplResult
 		}
 		return fn
 	})
