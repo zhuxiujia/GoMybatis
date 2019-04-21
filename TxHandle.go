f4d73f34ae8bcc0ee75242810b560521d30c16cc
zhuxiujia <zhuxiujia@qq.com>
2019-04-21 21:15:18 +0800

    add tx

--- TxHandle.go
+++ TxHandle.go
@@ -13,29 +13,43 @@ func AopProxyService(service reflect.Value, engine *GoMybatisEngine) {
 	var session Session
 	var txStack = tx.StructField{}.New()
 	ProxyValue(service, func(funcField reflect.StructField, field reflect.Value) func(arg ProxyArg) []reflect.Value {
-		//拷贝老方法，否则会循环调用导致栈溢出
+		//init data
+		var propagation = tx.PROPAGATION_NEVER
 		var nativeImplFunc = reflect.ValueOf(field.Interface())
+		var txTag, haveTx = funcField.Tag.Lookup("tx")
+		var rollbackTag, haveRollback = funcField.Tag.Lookup("rollback")
+		if haveTx {
+			propagation = tx.NewPropagation(txTag)
+		}
+		if haveRollback {
+			//todo dosthing
+		}
+
 		var fn = func(arg ProxyArg) []reflect.Value {
 			txStack.Push(funcField)
 			if txStack.Len() == 1 {
-				//PROPAGATION_REQUIRED
-				if session == nil {
-					//todo newSession is use service bean name?
-					var err error
-					session, err = engine.NewSession(beanName)
-					if err != nil {
-						panic(err)
-					}
-					err = session.Begin()
-					if err != nil {
-						panic(err)
+				if propagation == tx.PROPAGATION_NEVER{
+
+				} else if propagation == tx.PROPAGATION_REQUIRED {
+					//PROPAGATION_REQUIRED
+					if session == nil {
+						//todo newSession is use service bean name?
+						var err error
+						session, err = engine.NewSession(beanName)
+						if err != nil {
+							panic(err)
+						}
+						err = session.Begin()
+						if err != nil {
+							panic(err)
+						}
 					}
 				}
 			}
-			var nativeImplResult = doNativeMethod(arg,nativeImplFunc,session)
+			var nativeImplResult = doNativeMethod(arg, nativeImplFunc, session)
 			txStack.Pop()
 			if txStack.Len() == 0 {
-				if !haveError(nativeImplResult) {
+				if !haveRollBackType(nativeImplResult, rollbackTag) {
 					var err = session.Commit()
 					if err != nil {
 						panic(err)
@@ -53,7 +67,7 @@ func AopProxyService(service reflect.Value, engine *GoMybatisEngine) {
 	})
 }
 
-func doNativeMethod(arg ProxyArg,nativeImplFunc reflect.Value,session Session) []reflect.Value {
+func doNativeMethod(arg ProxyArg, nativeImplFunc reflect.Value, session Session) []reflect.Value {
 	defer func() {
 		err := recover()
 		if err != nil {
@@ -67,13 +81,13 @@ func doNativeMethod(arg ProxyArg,nativeImplFunc reflect.Value,session Session) [
 
 }
 
-func haveError(v []reflect.Value) bool {
+func haveRollBackType(v []reflect.Value, typeString string) bool {
 	if v == nil || len(v) == 0 {
 		return false
 	}
 	for _, item := range v {
 		if item.Kind() == reflect.Interface {
-			if item.String() == "error" {
+			if item.String() == typeString {
 				if !item.IsNil() {
 					return true
 				}
--- tx/Propagation.go
+++ tx/Propagation.go
@@ -4,7 +4,7 @@ package tx
 type Propagation int
 
 const (
-	PROPAGATION_REQUIRED      = iota //默认，表示如果当前事务存在，则支持当前事务。否则，会启动一个新的事务。xorm中默认事务类型。
+	PROPAGATION_REQUIRED      Propagation= iota //默认，表示如果当前事务存在，则支持当前事务。否则，会启动一个新的事务。xorm中默认事务类型。
 	PROPAGATION_SUPPORTS             //表示如果当前事务存在，则支持当前事务，如果当前没有事务，就以非事务方式执行。
 	PROPAGATION_MANDATORY            //表示如果当前事务存在，则支持当前事务，如果当前没有事务，则返回事务嵌套错误。
 	PROPAGATION_REQUIRES_NEW         //表示新建一个全新Session开启一个全新事务，如果当前存在事务，则把当前事务挂起。
@@ -13,3 +13,39 @@ const (
 	PROPAGATION_NESTED               //表示如果当前事务存在，则在嵌套事务内执行，如嵌套事务回滚，则只会在嵌套事务内回滚，不会影响当前事务。如果当前没有事务，则进行与PROPAGATION_REQUIRED类似的操作。
 	PROPAGATION_NOT_REQUIRED         //表示如果当前没有事务，就新建一个事务,否则返回错误。
 )
+
+func NewPropagation(arg string) Propagation {
+	switch arg {
+	case "":
+		return PROPAGATION_REQUIRED
+		break
+	case "REQUIRED":
+		return PROPAGATION_REQUIRED
+		break
+	case "SUPPORTS":
+		return PROPAGATION_SUPPORTS
+		break
+
+	case "MANDATORY":
+		return PROPAGATION_MANDATORY
+		break
+	case "REQUIRES_NEW":
+		return PROPAGATION_REQUIRES_NEW
+		break
+	case "NOT_SUPPORTED":
+		return PROPAGATION_NOT_SUPPORTED
+		break
+	case "NEVER":
+		return PROPAGATION_NEVER
+		break
+	case "NESTED":
+		return PROPAGATION_NESTED
+		break
+	case "NOT_REQUIRED":
+		return PROPAGATION_NOT_REQUIRED
+		break
+	default:
+		return PROPAGATION_REQUIRED
+	}
+	return PROPAGATION_REQUIRED
+}
