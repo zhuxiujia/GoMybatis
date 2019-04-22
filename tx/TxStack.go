113b404ea17c986e84dc5dd5a9ab0d0d2dcadfec
zhuxiujia <zhuxiujia@qq.com>
2019-04-22 22:39:51 +0800

    add tx

--- tx/TxStack.go
+++ tx/TxStack.go
@@ -19,14 +19,34 @@ func (s *TxStack) Push(k *sql.Tx) {
 	s.i++
 }
 
-func (s *TxStack) Pop() (ret *sql.Tx) {
+func (s *TxStack) Pop() *sql.Tx {
+	if s.i == 0 {
+		return nil
+	}
 	s.i--
-	ret = s.data[s.i]
+	var ret = s.data[s.i]
 	s.data = s.data[0:s.i]
-	return
+	return ret
+}
+func (s *TxStack) First() *sql.Tx {
+	if s.i == 0 {
+		return nil
+	}
+	var ret = s.data[0]
+	return ret
+}
+func (s *TxStack) Last() *sql.Tx {
+	if s.i == 0 {
+		return nil
+	}
+	var ret = s.data[s.i-1]
+	return ret
 }
 
 func (s *TxStack) Len() int {
 	return s.i
 }
 
+func (s *TxStack) HaveTx() bool {
+	return s.Len() > 0
+}
