0df84feef0c3c5043fe9db55ed87e1804c8504e4
zhuxiujia <zhuxiujia@qq.com>
2019-04-22 21:34:54 +0800

    add tx

--- utils/GoroutineUtil.go
+++ utils/GoroutineUtil.go
@@ -1,9 +1,22 @@
 package utils
 
 import (
-	"github.com/zhuxiujia/GoMybatis/lib/github.com/goroutineid"
+	"bytes"
+	"runtime"
+	"strconv"
 )
 
 func GoroutineID() int64 {
-	return goroutineid.GetGoID()
+//	return goroutineid.GetGoID()
+    return GetGID()
+}
+
+
+func GetGID() int64 {
+	b := make([]byte, 64)
+	b = b[:runtime.Stack(b, false)]
+	b = bytes.TrimPrefix(b, []byte("goroutine "))
+	b = b[:bytes.IndexByte(b, ' ')]
+	n, _ := strconv.ParseInt(string(b), 10, 64)
+	return n
 }
