0fb4ae68aaa827c4198a2792f60357444c9b04c2
zhuxiujia <zhuxiujia@qq.com>
2019-05-05 23:51:14 +0800

    add xml create util

--- XmlCreate.go
+++ XmlCreate.go
@@ -7,7 +7,7 @@ import (
 	"strings"
 )
 
-var XmlData = `<?xml version="1.0" encoding="UTF-8"?>
+var _XmlData = `<?xml version="1.0" encoding="UTF-8"?>
 <!DOCTYPE mapper PUBLIC "-//mybatis.org//DTD Mapper 3.0//EN"
         "https://raw.githubusercontent.com/zhuxiujia/GoMybatis/master/mybatis-3-mapper.dtd">
 <mapper>
@@ -20,10 +20,10 @@ var XmlData = `<?xml version="1.0" encoding="UTF-8"?>
     </resultMap>
 </mapper>
 `
-var XmlLogicEnable = `logic_enable="true" logic_undelete="1" logic_deleted="0"`
-var XmlVersionEnable = `version_enable="true"`
-var XmlIdItem = `<id column="id" property="id"/>`
-var ResultItem = `<result column="#{property}" property="#{property}" langType="#{langType}" #{version} #{logic} />`
+var _XmlLogicEnable = `logic_enable="true" logic_undelete="1" logic_deleted="0"`
+var _XmlVersionEnable = `version_enable="true"`
+var _XmlIdItem = `<id column="id" property="id"/>`
+var _ResultItem = `<result column="#{property}" property="#{property}" langType="#{langType}" #{version} #{logic} />`
 
 /**
 //例子
@@ -58,7 +58,7 @@ func CreateXml(tableName string, bean interface{}) []byte {
 	for i := 0; i < tv.NumField(); i++ {
 		var item = tv.Field(i)
 		if item.Name == "id" {
-			content += XmlIdItem
+			content += _XmlIdItem
 			content += "\n"
 		} else {
 			var itemName = item.Name
@@ -67,20 +67,20 @@ func CreateXml(tableName string, bean interface{}) []byte {
 				itemName = itemJson
 			}
 
-			var itemStr = strings.Replace(ResultItem, "#{property}", itemName, -1)
+			var itemStr = strings.Replace(_ResultItem, "#{property}", itemName, -1)
 			itemStr = strings.Replace(itemStr, "#{langType}", item.Type.Name(), -1)
 
 			var gm = item.Tag.Get("gm")
 			if gm != "" {
 				if gm == "id" {
-					content += XmlIdItem
+					content += _XmlIdItem
 					content += "\n"
 				}
 				if gm == "version" {
-					itemStr = strings.Replace(itemStr, "#{version}", XmlVersionEnable, -1)
+					itemStr = strings.Replace(itemStr, "#{version}", _XmlVersionEnable, -1)
 				}
 				if gm == "logic" {
-					itemStr = strings.Replace(itemStr, "#{logic}", XmlLogicEnable, -1)
+					itemStr = strings.Replace(itemStr, "#{logic}", _XmlLogicEnable, -1)
 				}
 			}
 			//clean
@@ -93,7 +93,7 @@ func CreateXml(tableName string, bean interface{}) []byte {
 			}
 		}
 	}
-	var res = strings.Replace(XmlData, "#{resultMapBody}", content, -1)
+	var res = strings.Replace(_XmlData, "#{resultMapBody}", content, -1)
 	res = strings.Replace(res, "#{table}", tableName, -1)
 	return []byte(res)
 }
