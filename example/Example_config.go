0fa3982b07e006659d132436454f78230f40dd0c
zhuxiujia <zhuxiujia@qq.com>
2020-02-11 17:42:22 +0800

    fix

--- example/Example_config.go
+++ example/Example_config.go
@@ -12,10 +12,10 @@ type Activity struct {
 	Id         string    `json:"id,omitempty"`
 	Uuid       string    `json:"uuid"`
 	Name       string    `json:"name"`
-	PcLink     string    `json:"pcLink"`
-	H5Link     string    `json:"h5Link"`
+	PcLink     string    `json:"pc_link"`
+	H5Link     string    `json:"h5_link"`
 	Remark     string    `json:"remark"`
 	Version    int       `json:"version"`
-	CreateTime time.Time `json:"createTime"`
-	DeleteFlag int       `json:"deleteFlag"`
+	CreateTime time.Time `json:"create_time"`
+	DeleteFlag int       `json:"delete_flag"`
 }
