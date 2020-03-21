2467674d0869106ad1bcaad4112bd08b53b9b87c
zhuxiujia <zhuxiujia@qq.com>
2020-03-21 22:57:25 +0800

    add test

--- ids/SnowFlake_test.go
+++ ids/SnowFlake_test.go
@@ -3,6 +3,7 @@ package ids
 import (
 	"fmt"
 	"runtime"
+	"sync"
 	"testing"
 )
 
@@ -15,16 +16,19 @@ func TestGetSnowflakeId(t *testing.T) {
 func TestSnowflakeDataRace(t *testing.T) {
 	var total = 100000
 	n, _ := NewNode(0)
+	var goruntine = runtime.NumCPU() * 2
+	var waitGroup = sync.WaitGroup{}
+	waitGroup.Add(goruntine)
+
 	for i := 0; i < runtime.NumCPU()*2; i++ {
 		go func() {
 			for i := 0; i < total; i++ {
 				n.Generate()
 			}
+			waitGroup.Done()
 		}()
 	}
-	for i := 0; i < total; i++ {
-		n.Generate()
-	}
+	waitGroup.Wait()
 }
 
 func BenchmarkGetSnowflakeId(b *testing.B) {
