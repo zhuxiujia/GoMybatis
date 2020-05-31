77f8117dae52e730af5575d63c2f61513039d8f8
zhuxiujia <zhuxiujia@qq.com>
2020-05-31 17:36:23 +0800

    fix stmt

--- example/Example_test.go
+++ example/Example_test.go
@@ -13,24 +13,6 @@ import (
 	"time"
 )
 
-//mysql链接格式为         用户名:密码@(数据库链接地址:端口)/数据库名称   例如root:root@(127.0.0.1:3306)/test?charset=utf8&parseTime=True&loc=Local
-//此处请按格式填写你的mysql链接，这里用*号代替
-const MysqlUri = "*"
-
-//定义数据库模型
-//例子：Activity 活动数据
-type Activity struct {
-	Id         string    `json:"id,omitempty"`
-	Uuid       string    `json:"uuid"`
-	Name       string    `json:"name"`
-	PcLink     string    `json:"pc_link"`
-	H5Link     string    `json:"h5_link"`
-	Remark     string    `json:"remark"`
-	Version    int       `json:"version"`
-	CreateTime time.Time `json:"create_time"`
-	DeleteFlag int       `json:"delete_flag"`
-}
-
 //支持基本类型和指针(int,string,time.Time,float...且需要指定参数名称`args:"name"以逗号隔开，且位置要和实际参数相同)
 //参数中包含有*GoMybatis.Session的类型，用于自定义事务，也可以选择例如TestService 这样使用更简单的声明式事务
 //自定义结构体参数（属性必须大写）
