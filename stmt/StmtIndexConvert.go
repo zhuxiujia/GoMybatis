db5c933af73ae9c92b3fafcd35c10c0723fd6b67
zhuxiujia <zhuxiujia@qq.com>
2020-05-31 17:35:23 +0800

    fix stmt

--- stmt/StmtIndexConvert.go
+++ stmt/StmtIndexConvert.go
@@ -1,5 +1,9 @@
 package stmt
 
+// stmt convert
+// example mysql: input 1 -> ?
+// oracle : input 0 ->   :val1
+// sqlite: input 0 ->   " ? "
 type StmtIndexConvert interface {
 	Convert(index int) string
 }
