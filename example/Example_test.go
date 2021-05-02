4a44947a430dde6b8dff8f11b2be3526aa23dc0e
zxj_pc <zhuxiujia@qq.com>
2021-05-02 22:22:33 +0800

    add example

--- example/Example_test.go
+++ example/Example_test.go
@@ -13,6 +13,26 @@ import (
 	"time"
 )
 
+//mysql链接格式为         用户名:密码@(数据库链接地址:端口)/数据库名称   例如root:root@(127.0.0.1:3306)/test?charset=utf8&parseTime=True&loc=Local
+//此处请按格式填写你的mysql链接，这里用*号代替
+const MysqlUri = "root:root@(127.0.0.1:3306)/test?charset=utf8&parseTime=True&loc=Local"
+
+//定义数据库模型
+//例子：Activity 活动数据,注意GoMybatis json tag 等价于数据库字段
+type Activity struct {
+	Id         string    `json:"id,omitempty"`
+	Uuid       string    `json:"uuid"`
+	Name       string    `json:"name"`
+	PcLink     string    `json:"pc_link"`
+	H5Link     string    `json:"h5_link"`
+	Remark     string    `json:"remark"`
+	Sort       int       `json:"sort"`
+	Status     int       `json:"status"`
+	Version    int       `json:"version"`
+	CreateTime time.Time `json:"create_time"`
+	DeleteFlag int       `json:"delete_flag"`
+}
+
 //支持基本类型和指针(int,string,time.Time,float...且需要指定参数名称`args:"name"以逗号隔开，且位置要和实际参数相同)
 //参数中包含有*GoMybatis.Session的类型，用于自定义事务，也可以选择例如TestService 这样使用更简单的声明式事务
 //自定义结构体参数（属性必须大写）
@@ -262,7 +282,7 @@ func Test_choose(t *testing.T) {
 		return
 	}
 	//使用mapper
-	var result, err = exampleActivityMapper.Choose(1)
+	var result, err = exampleActivityMapper.Choose(0)
 	if err != nil {
 		panic(err)
 	}
